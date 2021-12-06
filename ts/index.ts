146c8a7d329ae845bae48219daf6728d3dcbb813
devguchi <devguchitaro@gmail.com>
2021-12-07 00:48:37 +0900

    input data decode test

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,15 @@
 import { ethers } from 'ethers';
+const InputDataDecoder = require('ethereum-input-data-decoder');
+const abi = [{}];
+const inputData = '0xxxx';
+const decoder = new InputDataDecoder(abi);
+const result = decoder.decodeData(inputData);
+
+const bn = result.inputs[0];
+const bytes32 = new Uint8Array(result.inputs[1]);
+const bytes = new Uint8Array(result.inputs[2]);
+console.log(bn.toString(10), bytes32, bytes);
+const bytes32Hex = Buffer.from(bytes32).toString("hex");
+const bytesHex = Buffer.from(bytes).toString("hex");
+console.log(bytes32Hex);
+console.log(bytesHex);
