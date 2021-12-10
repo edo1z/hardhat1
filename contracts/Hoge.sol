2701ed7ff84182653a7a8fd4a90075ea26aaffde
web3ten0 <web3ten0@gmail.com>
2021-12-10 22:55:45 +0900

    payable test

--- contracts/Hoge.sol
+++ contracts/Hoge.sol
@@ -2,16 +2,8 @@
 pragma solidity ^0.8.0;
 
 contract Hoge {
-    uint256 x = 1;
-
-    fallback() external {
-        x = 1;
-    }
-
-    function a(uint256 b) external returns (uint256) {
-        require(x < 10, 'invalid x');
-        uint256 ans = x * b;
-        x++;
-        return ans;
+    receive() external payable {}
+    function getContractBalance() external view returns (uint) {
+        return address(this).balance;
     }
 }
