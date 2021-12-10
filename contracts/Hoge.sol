f53d1636542ab34d048a4a6155e252ab8608b9d7
web3ten0 <web3ten0@gmail.com>
2021-12-11 00:16:20 +0900

    event test

--- contracts/Hoge.sol
+++ contracts/Hoge.sol
@@ -1,9 +1,15 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Hoge {
-    receive() external payable {}
-    function getContractBalance() external view returns (uint) {
+    event Deposit(address indexed _from, uint256 _value, uint256 _balance);
+
+    receive() external payable {
+        uint256 balance = getContractBalance();
+        emit Deposit(msg.sender, msg.value, balance);
+    }
+
+    function getContractBalance() public view returns (uint256) {
         return address(this).balance;
     }
 }
